628ace9711a1feaa7d259f7aca7e8bb271a44053
Charlie Birks <admin@daftgames.net>
2014-05-18 16:17:34 +0100

    add power support

--- src/power/SDL_power.c
+++ src/power/SDL_power.c
@@ -37,6 +37,7 @@ SDL_bool SDL_GetPowerInfo_Haiku(SDL_PowerState *, int *, int *);
 SDL_bool SDL_GetPowerInfo_UIKit(SDL_PowerState *, int *, int *);
 SDL_bool SDL_GetPowerInfo_Android(SDL_PowerState *, int *, int *);
 SDL_bool SDL_GetPowerInfo_PSP(SDL_PowerState *, int *, int *);
+SDL_bool SDL_GetPowerInfo_Emscripten(SDL_PowerState *, int *, int *);
 
 #ifndef SDL_POWER_DISABLED
 #ifdef SDL_POWER_HARDWIRED
@@ -76,6 +77,9 @@ static SDL_GetPowerInfo_Impl implementations[] = {
 #endif
 #ifdef SDL_POWER_PSP        /* handles PSP. */
     SDL_GetPowerInfo_PSP,
+#endif
+#ifdef SDL_POWER_EMSCRIPTEN     /* handles Emscripten */
+    SDL_GetPowerInfo_Emscripten,
 #endif
 
 #ifdef SDL_POWER_HARDWIRED
